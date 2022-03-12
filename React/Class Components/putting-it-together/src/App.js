5633cdfa659cd7621056596ef95f5c242b8910d1
Jonathan Smith <jonathan.smith722@gmail.com>
2022-03-12 12:12:50 -0800

    Updated 03/12/22

--- React/Class Components/putting-it-together/src/App.js
+++ React/Class Components/putting-it-together/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>This is a child</p>
         <p>This is another child</p>
         <p>This is even another child</p>
+        <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
       </Together>
     </div>
   );
--- React/Class Components/putting-it-together/src/components/Together.jsx
+++ React/Class Components/putting-it-together/src/components/Together.jsx
@@ -1,9 +1,24 @@
 import React, {Component} from "react"
 
 class Together extends Component{
+    // Declare constructor to override our default constructor so that we can initially set the state object
+    constructor(props){
+        // gives us all the functionality of the default costructor that comes with Component
+        super(props)
+        this.state = {
+            message: "Welcome to the site"
+        }
+    }
+
+    logout = (e)=> {
+        this.setState({message: "Goodbye Friend"})
+    }
+
     render(){
         return(
             <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={this.logout}>Leave</button>
                 <h1>
                     { this.props.header }
                 </h1>
