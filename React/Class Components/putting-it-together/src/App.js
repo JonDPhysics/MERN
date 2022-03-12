7c8f97ad32da72f3acce2c4198b29c4a298e041f
Jonathan Smith <jonathan.smith722@gmail.com>
2022-03-12 11:33:22 -0800

    Updated 03/12/22

--- React/Class Components/putting-it-together/src/App.js
+++ React/Class Components/putting-it-together/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Together from './components/Together'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Together header ={ "Header Prop" }>
+        <p>This is a child</p>
+        <p>This is another child</p>
+        <p>This is even another child</p>
+      </Together>
     </div>
   );
 }
