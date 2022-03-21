6554252c2157ff99ba0c4f3ba1ad82500947cf42
Jonathan Smith <jonathan.smith722@gmail.com>
2022-03-21 16:31:17 -0700

    Update 03/21/22

---
+++ MongoDB/Mongoose/Jokes/server/server.js
@@ -0,0 +1,16 @@
+// import all dependencies
+const express = require('express')
+const app = express()
+const dbName = "jokes_db"
+const port = 8000
+
+// config mongoose
+require("./configs/mongoose.config")(dbName)
+
+// express config for post
+app.use(express.json(), express.urlencoded({ extended: true }))
+
+// get the routes
+
+// listen to the port
+app.listen(port, () => console.log(`Listening on port ${port} for request and response.`))
