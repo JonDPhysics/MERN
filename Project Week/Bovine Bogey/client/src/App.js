490e3ece5e1b0fa33d915f5b705ddda59aa26266
Jonathan Smith <jonathan.smith722@gmail.com>
2022-04-04 08:32:21 -0700

    updated 04/04/22

--- Project Week/Bovine Bogey/client/src/App.js
+++ Project Week/Bovine Bogey/client/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { BrowserRouter, Switch, Router }
+import { BrowserRouter, Switch, Router } from "react-router-dom"
+
 
 function App() {
+
   return (
+    
     <BrowserRouter>
-      
+      <fieldset>
+        <legend><h1>Bovine Bogey</h1></legend>
+        <Switch>
+          <Route exact path="/">
+            <Bogey/>
+          </Route>
+          <Route exact path = "/bogey/add">
+            <Add/>
+          </Route>
+          <Route exact path ="/bogey/care">
+            <Care/>
+          </Route>
+        </Switch>
+      </fieldset>
     </BrowserRouter>
   );
 }
---
+++ Project Week/Bovine Bogey/client/src/views/Bogey.jsx
@@ -0,0 +1,8 @@
+import React, { useEfect, useState } from "react";
+import axios from "axios"
+import { Link, useHistory } from 'react-router-dom'
+
+const Bogey = () => {
+    const [bovine, setBovine] = useState([])
+    
+}
